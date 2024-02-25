baf61b1b35445d595bbad8be8af842b2a7a659bc
goeflo <florian@goeldenitz.org>
2024-02-25 23:10:58 +0100

    remove no longer needed code

--- create_season.sh
+++ create_season.sh
@@ -6,3 +6,5 @@ curl -X POST http://localhost:8081/event/season/1/race -H 'Content-Type: applica
 curl --data "/home/florian/work/srcm/race01-results.csv" http://localhost:8081/event/race/1/results
 
  curl -X POST -d '{"email":"admin","passwd":"1234"}' localhost:8080/api/v1/event/season -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluIn0.A9dz8H4vRCdMb39m6nOlnl_HbF5zgof5LrLm2i0xEY0"
+
+  curl -X POST -d '{"raceName":"my race","seasonName":"huch"}' localhost:8080/api/v1/event/race -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluIn0.A9dz8H4vRCdMb39m6nOlnl_HbF5zgof5LrLm2i0xEY0"
